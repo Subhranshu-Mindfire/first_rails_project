6482adb45c858f57ccd2741d397c3462fde51b86
subhranshu <subhranshu.mfs@gmail.com>
2025-01-24 17:29:34 +0530

    added mail configs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,15 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         roles = role_params[:roles]
         
-        if roles != nil
-          @user.roles = Role.where(id:roles.map{|role| role.to_i})
+
+        if roles.present?
+          roles = roles.map!{|role| role.to_i}
+          @user.roles = Role.where(id: roles)
         end
 
         if @user.save
-            redirect_to users_path, notice: "User Created Successfully"
+          UserMailer.welcome_email(@user).deliver_now
+          redirect_to users_path, notice: "User Created Successfully"
         else
             flash.now[:alert] = @user.errors.full_messages.to_sentence
             # render turbo_stream: [turbo_stream.update("flash", partial: "shared/flash")]
@@ -47,21 +50,31 @@ class UsersController < ApplicationController
         begin
           if user.update(user_params)
             roles = role_params[:roles]
-            
-            if roles != nil
-              roles.each do |role|
-            
-                if not(user.roles.map{ |rol| rol.id}.include?(role.to_i))
-                  UserRole.create(user_id:@user.id, role_id:role)
-                end
-  
-                (user.roles.map{ |rol| rol.id} - roles.map{|ro| ro.to_i}).each do |r|
-                  UserRole.find_by(user_id:user.id, role_id:r).destroy
-                end
-  
-              end
+
+            if roles.blank?
+              flash.now[:alert] = "User must have atleast one role"
+              render :edit, status: :unprocessable_entity
+              return
             end
+
+            roles.map!{ |role| role.to_i }
+            user.update(role_ids: roles)
+            
+            # roles.each do |role|
+            #   user.user_roles
+            #   if !(user.role_ids.include?(role.to_i))
+            #     UserRole.create(user_id:@user.id, role_id:role)
+            #   end
+
+            #   (user.roles.map{ |rol| rol.id} - roles.map{|ro| ro.to_i}).each do |r|
+            #     if UserRole.find_by(user_id:user.id, role_id:r)
+            #       UserRole.find_by(user_id:user.id, role_id:r).destroy
+            #     end
+            #   end
+            # end
             redirect_to users_path,notice:"User Updated Successfully"
+            
+            
             
           else
             flash.now[:alert] = @user.errors.full_messages.to_sentence
@@ -73,13 +86,18 @@ class UsersController < ApplicationController
         end
     end
 
-    def destroy
+    def destroy      
       begin
-        user.roles.map{ |role| role.id }.each do |ro|
-            UserRole.find_by(user_id:user.id,role_id:ro).destroy() 
-        end
-        user.destroy()
-        redirect_to users_path, notice:"User Deleted Successfully"
+
+        # owner_role = Role.where(title: "Owner").first
+        # is_owner = user.roles.where(roles: {id: owner_role.id}).present?
+  
+        # if is_owner
+        #   redirect_to users_path, alert:"Owner Can not be deleted"
+        # else
+          # user.destroy
+          redirect_to users_path, notice:"Testing"     
+        # end   
       rescue ActiveRecord::RecordNotFound => e
           redirect_to '/404'
       end
@@ -97,6 +115,9 @@ class UsersController < ApplicationController
     def user
         @user ||= User.find(params[:id])
     end
+
+    
+
  
   end
 
